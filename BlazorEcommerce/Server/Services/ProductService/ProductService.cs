8d6120cfec14f94923f40da28efbd47a35098404
Romulo Lima <romulosilva-07@hotmail.com>
2023-04-23 22:43:33 -0300

    created a product details page

--- BlazorEcommerce/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -7,6 +7,25 @@
         {
             _context = context;
         }
+
+        public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
+        {
+            var response = new ServiceResponse<Product>();
+            var product = await _context.Products.FindAsync(productId);
+
+            if (product == null)
+            {
+                response.Success = false;
+                response.Message = "Desculpa, mas esse produto não existe.";
+            }
+            else
+            {
+                response.Data = product;
+            }
+
+            return response;
+        }
+
         public async Task<ServiceResponse<List<Product>>> GetProductsAsync()
         {
             var response = new ServiceResponse<List<Product>>
