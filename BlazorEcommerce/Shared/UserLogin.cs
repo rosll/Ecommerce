763382e4912b018aa85888b18ef131a5ae64d129
Romulo Lima <romulosilva-07@hotmail.com>
2023-07-03 02:15:28 -0300

    jwt and login features

--- BlazorEcommerce/Shared/UserLogin.cs
+++ BlazorEcommerce/Shared/UserLogin.cs
@@ -9,6 +9,9 @@ namespace BlazorEcommerce.Shared
 {
     public class UserLogin
     {
-        
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
     }
 }
